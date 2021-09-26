dcd947b060b5c00927a3614cf2ec4283292911dd
Suchintha-Git <86240934+Suchintha-Git@users.noreply.github.com>
2021-09-26 15:35:26 +0530

    store java class added

---
+++ app/src/main/java/com/example/work_byte/SQLiteHelper.java
@@ -0,0 +1,91 @@
+package com.example.imagecrud;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteHelper extends android.database.sqlite.SQLiteOpenHelper {
+
+
+    public SQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public void queryData(String sql){
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    public void insertData(String itemname, String discription, byte[] image, String phone, String price){
+
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO FOOD VALUES (NULL, ?, ?, ?, ?, ?)";
+
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString(1, itemname);
+        statement.bindString(2, discription);
+        statement.bindBlob(3, image);
+        statement.bindString(4, phone);
+        statement.bindString(5, price);
+
+
+        statement.executeInsert();
+
+    }
+
+    //Updating data into sqlite
+    public void updateData(String itemname, String discription, byte[] image, String phone, String price, int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "UPDATE FOOD SET name = ?, discription = ?, image = ?, phone = ?,price = ? WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+
+        statement.bindString(1, itemname);
+        statement.bindString(2, discription);
+        statement.bindBlob(3, image);
+        statement.bindString(4, phone);
+        statement.bindString(5, price);
+        statement.bindDouble(6, (double)id);
+
+        statement.execute();
+        database.close();
+    }
+
+
+    //Deleting data in sqlite with id field
+    public  void deleteData(int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "DELETE FROM FOOD WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+        statement.bindDouble(1, (double)id);
+
+        statement.execute();
+        database.close();
+    }
+
+
+    //get all data from database
+    public Cursor getData(String sql){
+
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql, null);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
